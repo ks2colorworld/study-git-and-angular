f11e2e1257fc12681850ddc2c2a9fcbd0b010482
kslee <ks@colorworld.kr>
2019-08-20 18:22:47 +0900

    update ng s -c=detail 테스트 코드 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-project-management-with-github';
+  //title = 'angular-project-management-with-github';
+  addTitle = environment.detailMessage? ' with detail message':'!';
+  title = environment.server_url.toString() + this.addTitle ;
 }
