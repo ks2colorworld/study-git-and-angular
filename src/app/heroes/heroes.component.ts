8eaa564e436d0c5bd68f7e5a1357dfeb6e78c1d3
kslee <ks@colorworld.kr>
2019-08-29 23:32:12 +0900

    hero class 추가 > 디테일 데이터 추가

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  hero: Hero = {
+    id:1,
+    name:'Windstorm',
+  }
 
   constructor() { }
 
