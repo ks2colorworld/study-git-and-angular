edda423ec413d5e08ab69f158740b9a1bff851f5
kslee <ks@colorworld.kr>
2019-08-30 04:03:59 +0900

    메인 app component에서 라우터로 설정된 컴포넌트가 표시될 부분을 router-outlet 태그로 지정함.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path:'heroes', component:HeroesComponent}
+  {path:'heroes', component:HeroesComponent} // 인터넷주소창에 (root주소)/heroes 가 입력되면 해당 컴포넌트를 표시한다.
 ];
 
 @NgModule({
