ca8539d867ea05e42602eebe895b2ea6a8d09ac8
kslee <ks@colorworld.kr>
2019-08-30 01:34:27 +0900

    목업 데이터를 기준으로 선택된 항목 데이터 바인딩 + environment 설정값 활용하는 테스트 코드 추가 ng-container 사용

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from "../mock-heroes";
@@ -13,6 +14,17 @@ export class HeroesComponent implements OnInit {
   hero: Hero = {
     id:1,
     name:'Windstorm',
+  }
+
+  selectedHero:Hero;
+  onSelect(hero: Hero): void{
+    /* environment 사용 확인 : import { environment } from './../../environments/environment';
+    console.log('environment.detailMessage : ' + environment.detailMessage);
+    console.log('environment.production : ' + environment.production);
+    console.log('environment.server_url : ' + environment.server_url);
+    //*/
+    if(environment.detailMessage) console.log(hero);
+    this.selectedHero = hero;
   }
 
   constructor() { }
