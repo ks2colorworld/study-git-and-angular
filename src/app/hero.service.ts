902a29319798f4ea195bb07353c69abe6a643975
kslee <ks@colorworld.kr>
2019-08-30 03:33:01 +0900

    hero.service에 messageService를 주입함.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
 import { Observable, of } from "rxjs";
 import { delay } from "rxjs/operators";
+import { MessageService } from "./message.service";
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,12 @@ import { delay } from "rxjs/operators";
 export class HeroService {
 
   getHeroes(): Observable<Hero[]>{
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES).pipe(
       delay(1500) // 1.5초 - 데이터 로딩이 늦는 경우를 강제 구현해 봄.
     );
   }
-  constructor() { }
+  constructor(
+    private messageService: MessageService,
+  ) { }
 }
