9e57ea3f74047a9fb8b43232a5754b3f236ed839
kslee <ks@colorworld.kr>
2019-08-30 10:35:09 +0900

    scss 문법이 익숙하지 않고, 샘플 코드가 css 기준이라 부득이하게 변경함.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,7 +6,7 @@ import { HeroService } from "../hero.service";
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
