27d96a8622597dff10c1fbb867ca979037ad64fe
kslee <ks@colorworld.kr>
2019-08-30 12:18:09 +0900

    /detail/:id 타깃 컴포넌트를 새로 작성한 컨테이너로 변경함.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDetailForRouterComponent } from './hero-detail-for-router/hero-detail-for-router.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
   {path:'', redirectTo:'/dashboard', pathMatch:'full'}, //대응: (root url) >> 주소전환 (root url)/dashboard
   {path:'heroes', component:HeroesComponent}, // 인터넷주소창에 (root주소)/heroes 가 입력되면 해당 컴포넌트를 표시한다.
   {path:'dashboard', component: DashboardComponent}, // 대응 : (root url)/dashboard
-  {path:'detail/:id', component:HeroDetailComponent}, // 대응예시 : (root url)/detail/11 >> 'id = 11'로 사용
+  {path:'detail/:id', component:HeroDetailForRouterComponent}, // 대응예시 : (root url)/detail/11 >> 'id = 11'로 사용
 ];
 
 @NgModule({
