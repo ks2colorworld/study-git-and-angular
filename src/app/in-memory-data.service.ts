91aba860aa807045164c2ef9f257101029c6fd12
kslee <ks@colorworld.kr>
2019-08-31 08:43:03 +0900

    in memory db 테스트를 위한 코드 추가

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,6 +1,7 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 import { Hero } from "./hero";
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,17 +9,18 @@ import { Injectable } from '@angular/core';
 export class InMemoryDataService implements InMemoryDbService {
 
   createDb(){
+    /* 데이터와의 작동 방식을 확인하기 위해 변경해 봄. */
     const heroes1 = [
-      {id:11, name: 'Dr Nice'},
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' },
+      { id: 11, name: 'Dr Nice', age: 20, place: 'us'},
+      { id: 12, name: 'Narco', age: 30, place: 'kr' },
+      { id: 13, name: 'Bombasto', age: 40, place: 'uk' },
+      { id: 14, name: 'Celeritas', age: 15, place: 'us' },
+      { id: 15, name: 'Magneta', age: 60, place: 'jp' },
+      { id: 16, name: 'RubberMan', age: 70, place: 'cn' },
+      { id: 17, name: 'Dynama', age: 44, place: 'dh' },
+      { id: 18, name: 'Dr IQ', age: 38, place: 'uk' },
+      { id: 19, name: 'Magma', age: 25, place: 'sw' },
+      { id: 20, name: 'Tornado', age: 58, place: 'hk' },
     ];
     const heroes2 = [
       { id: 111, name: 'new Dr Nice'},
@@ -32,11 +34,16 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 119, name: 'new Magma' },
       { id: 120, name: 'new Tornado' },
     ];
-
+    if(environment.detailMessage){
+    console.log(heroes1);
+    console.log(heroes2);
+    }
     return {heroes1, heroes2};
   }
 
   genId(heroes: Hero[]): number{
+    if(environment.detailMessage)
+    console.log(heroes);
     return heroes.length > 0?
       Math.max(...heroes.map(hero => hero.id)) + 1 :
       11;
