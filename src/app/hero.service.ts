44854e856793e3034b74eb1ba3335227d75b5e14
kslee <ks@colorworld.kr>
2019-08-30 12:13:13 +0900

    id > hero 정보 가져오는 service 추가

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { HEROES } from "./mock-heroes";
 import { Observable, of } from "rxjs";
 import { delay } from "rxjs/operators";
 import { MessageService } from "./message.service";
+import { environment } from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,22 @@ import { MessageService } from "./message.service";
 export class HeroService {
 
   getHeroes(): Observable<Hero[]>{
+    if(environment.detailMessage)
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES).pipe(
-      delay(1500) // 1.5초 - 데이터 로딩이 늦는 경우를 강제 구현해 봄.
+      delay(800) // 0.8초 - 데이터 로딩이 늦는 경우를 강제 구현해 봄.
     );
   }
+  getHero(id: number): Observable<Hero> {
+    if(environment.detailMessage)
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(
+      hero => hero.id === id)
+      ).pipe(
+        delay(800) // 0.8초 - 데이터 로딩 중인 척
+      );
+  }
+
   constructor(
     private messageService: MessageService,
   ) { }
