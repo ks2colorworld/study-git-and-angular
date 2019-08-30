594101c69d794d5f7bac34ac67e14a29659329bc
kslee <ks@colorworld.kr>
2019-08-30 11:10:00 +0900

    dashboard component 추가 > router 추가 > routerLink 추가

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path:'heroes', component:HeroesComponent} // 인터넷주소창에 (root주소)/heroes 가 입력되면 해당 컴포넌트를 표시한다.
+  {path:'', redirectTo:'/dashboard', pathMatch:'full'}, //대응: (root url) >> 주소전환 (root url)/dashboard
+  {path:'heroes', component:HeroesComponent}, // 인터넷주소창에 (root주소)/heroes 가 입력되면 해당 컴포넌트를 표시한다.
+  {path:'dashboard', component: DashboardComponent}, // 대응 : (root url)/dashboard
 ];
 
 @NgModule({
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,28 @@
+import { HeroService } from './../hero.service';
+import { Hero } from './../hero';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  heroes:Hero[] = [];
+  getHeroes(): void{
+    this.heroService.getHeroes()
+      .subscribe(
+        heroes => this.heroes = heroes.slice(1,5)
+        );
+  }
+
+  constructor(
+    private heroService: HeroService,
+  ) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
